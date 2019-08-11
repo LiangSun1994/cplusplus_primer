a05a88c50b0135ca2f9ca4fa78494fd77eb0dad4
e <1373757850@qq.com>
2019-08-11 15:31:50 +0800

    add chapter_4_8_using string

---
+++ chapter_4/chapter_4_8_strtype2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+/*
+int main(){
+    using namespace std;
+    string s1 = "penguin";
+    string s2,s3;
+
+    //#1.assign
+    cout<<" you can assign one string to another"<<endl;
+    s2 = s1;
+    cout<<"s1: "<<s1<<" s2:"<<s2<<endl;
+
+    cout<<"s2 = \"buzzard\""<<endl;
+    s2 = "buzzard";
+    cout<<"s2: "<<s2<<endl;
+
+    //#2.concatenate
+    cout<<"you can concatenate string s3= s1+s2"<<endl;
+    s3 = s1 +s2;
+    cout<<s3<<endl;
+}
+*/
