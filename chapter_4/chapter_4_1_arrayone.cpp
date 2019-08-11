f8bb0291610a7238c8c6e1b386137dd640b1ff77
e <1373757850@qq.com>
2019-08-11 12:11:13 +0800

    modify chapter_4_1

--- chapter_4/chapter_4_1_arrayone.cpp
+++ chapter_4/chapter_4_1_arrayone.cpp
@@ -8,7 +8,8 @@ int main(){
     yams[2] = 40;
     cout<<"total yams equils = "<< yams[0] + yams[1] + yams[2] <<endl;
 
+    int yams_costs[2] = {1,2};//可以在声明的时候初始化变量的值
     cout<<"size of yams array" << sizeof(yams)<<endl;
     cout<<"siz of yams array element" << sizeof(yams[0])<<endl;
 }
- */
+*/
