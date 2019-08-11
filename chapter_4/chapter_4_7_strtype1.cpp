009c74efd3536d1b29b45d638b30532874dc52a0
e <1373757850@qq.com>
2019-08-11 15:25:48 +0800

    add chater_4_7_strtype

---
+++ chapter_4/chapter_4_7_strtype1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+/*
+int main(){
+    using namespace std;
+
+    char charr1[20];
+    char charr2[20] = "jaguar";
+
+    string strr1;
+    string strr2 = "pather";
+
+    cout<<"plaese enter an feline"<<endl;
+    cin>>charr1;
+    cout<<"please enter an another feline"<<endl;
+    cin>>strr1;
+    cout<<"Here are two feline, one is "<<charr1<<" another is "<<strr1<<endl;
+}
+*/
