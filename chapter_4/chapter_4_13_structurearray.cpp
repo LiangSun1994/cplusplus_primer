aa039a9bfd6cdff9d988d653f78df2c8623cb923
e <1373757850@qq.com>
2019-08-11 23:03:42 +0800

    add structure array

---
+++ chapter_4/chapter_4_13_structurearray.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+using namespace std;
+struct  inflatable {
+    char name[20];
+    float volumn;
+    double price;
+};
+int main(){
+    inflatable guest[2] = {
+            {"sun", 1.2, 2.3},
+            {"liang", 1.3 ,3.4}
+    };
+    cout<<"two guests "<< guest[0].name << " and "<<guest[1].name<<
+    " have a combined volume of "<<guest[0].volumn+guest[1].volumn<<endl;
+}
+*/
