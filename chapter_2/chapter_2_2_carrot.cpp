b59415c25bead765c7ae0eaa372b86cd61185c35
e <1373757850@qq.com>
2019-08-11 10:15:07 +0800

    motify

---
+++ chapter_2/chapter_2_2_carrot.cpp
@@ -0,0 +1,10 @@
+//  carrt_2_2.cpp --uses and display a variable
+
+#include <iostream>
+
+int main(){
+    using namespace std;
+    int carrots;
+    carrots = 25;
+    cout<< "I have "<< carrots << "carrots";
+}
