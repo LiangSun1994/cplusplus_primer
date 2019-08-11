c7b945b5e1a578b5a948f55f3597e97644c03b1f
e <1373757850@qq.com>
2019-08-11 14:51:16 +0800

    add instr2

--- chapter_4/chapter_4_2_string.cpp
+++ chapter_4/chapter_4_2_string.cpp
@@ -12,8 +12,9 @@ int main(){
     cout<<"what's your name?"<<endl;
     cin >> name1;
     cout<< "well your name "<< name1<< " has "<<sizeof(name1)<<"letters"<<endl;
+    cout<<"your name contain " <<strlen(name1)<<"bytes"<<endl;
 
     name2[5] = '\0';
     cout<<name2<<endl;
 }
- */
+*/
---
+++ chapter_4/chapter_4_4_instr2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    using namespace std;
+    const int Size = 20;
+    char name1[Size];
+    char desert[Size];
+
+    cout<<"enter your name"<<endl;
+    cin>> name1;
+
+    cout<<"enter your favrite dessert"<<endl;
+    cin >>desert;
+    cout<<"I have a desert "<<desert<< " for you "<< name1<<endl;
+}
