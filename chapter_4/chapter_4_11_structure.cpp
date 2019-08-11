f2464ec811f5e9ef5db59915d743eb9a61ba199f
e <1373757850@qq.com>
2019-08-11 16:37:42 +0800

    how to initalization structure

---
+++ chapter_4/chapter_4_11_structure.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+/*
+struct inflatable{
+    char name[20];
+    float volume;
+    double price;
+};
+int main(){
+
+    inflatable guest ={
+            "liang",
+            1.88,
+            2.99
+    };
+    inflatable pal = {
+            "sun",
+            1.22,
+            2.0
+    };
+
+    cout<<"you can expand you guest name:"<<guest.name<< " and "<< pal.name<<endl;
+    cout<<"you can have both for "<<guest.price+pal.price<<endl;
+}
+*/
