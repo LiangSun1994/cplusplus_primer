49bafb9d4ccd78da55b7e34ad433223fa8909dfb
e <1373757850@qq.com>
2019-08-11 11:23:43 +0800

    add chapter_2_6

---
+++ chapter_2/chapter_2_6_convert.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int stone2lb(int stone);
+/*
+int main(){
+    using namespace std;
+    int stone = 15;
+    cout<<"stone = "<<stone<<endl;
+    int pounds = stone2lb(stone);
+    cout<<"pounds= "<<pounds<<endl;
+}
+
+int stone2lb(int stone){
+    return stone*12;
+}
+ */
