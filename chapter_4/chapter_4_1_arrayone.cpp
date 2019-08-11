172e92780544e1ff99946c38b82449b398ac9a5e
e <1373757850@qq.com>
2019-08-11 12:08:59 +0800

    add  chapter_4_1_arrayone

---
+++ chapter_4/chapter_4_1_arrayone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/*
+int main(){
+    int yams[3];
+    yams[0] =20;
+    yams[1] = 30;
+    yams[2] = 40;
+    cout<<"total yams equils = "<< yams[0] + yams[1] + yams[2] <<endl;
+
+    cout<<"size of yams array" << sizeof(yams)<<endl;
+    cout<<"siz of yams array element" << sizeof(yams[0])<<endl;
+}
+ */
