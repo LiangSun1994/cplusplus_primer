72ed4a03b48368bdfe3552b4ad7dc333d8d43478
e <1373757850@qq.com>
2019-08-11 15:01:09 +0800

    add using cin.get to load string

---
+++ chapter_4/chapter_4_5_instr3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+/*
+int main(){
+    using namespace std;
+
+    const int Size = 20;
+    char name[Size];
+    char desert[Size];
+
+    cout<<"please enter your name"<<endl;
+    cin.get(name, Size).get();
+    cout<<"please enter you favorite dessert"<<endl;
+    cin.get(desert, Size).get();
+    cout<<"I have a dessert "<<desert<< " for you "<< name<<endl;
+    return 0;
+}
+*/
