4beea649362fb8133c667088342f6f5e75823ddf
e <1373757850@qq.com>
2019-08-11 10:37:46 +0800

    add chapter_2_3

---
+++ chapter_2/chapter_2_3_getinfo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+/*
+int main(){
+    using namespace std;
+    int carrots;
+
+    cout<<"how many carrots do you have?"<<endl;
+    cin >> carrots;
+    cout<< "i have "<< carrots<<endl;
+
+}
+ */
