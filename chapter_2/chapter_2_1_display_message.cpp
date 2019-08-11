c564e7e0c4980b5663cc124905c07e0a0c8df870
e <1373757850@qq.com>
2019-08-11 10:20:52 +0800

    modify

--- chapter_2/chapter_2_1_display_message.cpp
+++ chapter_2/chapter_2_1_display_message.cpp
@@ -1,9 +1,10 @@
 // myfirst.cpp -- disaplay a message
 
 #include <iostream>
-
+/*
 int main(){
     using namespace std;
     cout<<"come up and c++ me some time"<<endl;
     return 0;
 }
+*/
--- chapter_2/chapter_2_2_carrot.cpp
+++ chapter_2/chapter_2_2_carrot.cpp
@@ -1,10 +1,13 @@
 //  carrt_2_2.cpp --uses and display a variable
 
 #include <iostream>
-
+/*
 int main(){
     using namespace std;
     int carrots;
+
     carrots = 25;
+    cout<<carrots<<endl;
     cout<< "I have "<< carrots << "carrots";
 }
+ */
