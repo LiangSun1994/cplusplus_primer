07e03ff1bd03f1a459913da581f7ff0f4a75e522
e <1373757850@qq.com>
2019-08-11 22:45:47 +0800

    add struct assgin

---
+++ chapter_4/chapter_4_12_assign_structure.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/*
+struct inflatable{
+    char name[20];
+    float volume;
+    double price;
+};
+int main(){
+    inflatable bouquet = {
+            "sunflowers",
+            0.2,
+            1.3
+    };
+    inflatable choice;
+    cout<<"bouquent.name:  "<<bouquet.name<<endl;
+    cout<<"bouquenet.price  "<<bouquet.price<<endl;
+    choice = bouquet;
+    cout<<"choice.name  "<<choice.name<<endl;
+    cout<<"choice.price  "<<choice.price<<endl;
+}
+*/
