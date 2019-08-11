aa87836eba2be6e34a1efaff6674dbf2eff36b64
e <1373757850@qq.com>
2019-08-11 11:00:10 +0800

    add chapter_2_4 sqrt

--- chapter_2/chapter_2_3_getinfo.cpp
+++ chapter_2/chapter_2_3_getinfo.cpp
@@ -9,4 +9,4 @@ int main(){
     cout<< "i have "<< carrots<<endl;
 
 }
- */
+*/
---
+++ chapter_2/chapter_2_4_sqrt.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    using namespace std;
+
+    double area;
+    cout<<"please enter area"<<endl;
+    cin >> area;
+
+    double side;
+    side = sqrt(area);
+    cout<<"area is "<<side<<endl;
+
+}
