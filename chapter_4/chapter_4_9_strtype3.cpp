ad570992e405a7568820638ae77d3d42db45483d
e <1373757850@qq.com>
2019-08-11 15:42:46 +0800

    add chapter_4_9

---
+++ chapter_4/chapter_4_9_strtype3.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+/*
+int main(){
+    using namespace std;
+    char charr1[20];
+    char charr2[20] = "penguin";
+
+    string str;
+    string str2 = "pather";
+
+    //1. assign
+    str = str2;
+    strcpy(charr1,charr2);
+
+    //2. concatenate
+    str += "yo";
+    strcat(charr1, "yo");
+
+    //3.lenght
+    int len1 = strlen(charr1);
+    int len2 = str.size();
+
+    cout<<"charr1:" <<charr1<<" len1 = "<<len1<<endl;
+    cout<<"str:" <<str<< "len2 = "<<len2<<endl;
+}
+*/
