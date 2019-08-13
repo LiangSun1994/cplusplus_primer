68ea2908f7b61c1a87e83589399e90eddd222f92
e <1373757850@qq.com>
2019-08-13 23:26:08 +0800

    add chapter new

---
+++ chapter_4/chapter_4_17_using_new.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main(){
+
+  int nights = 10;
+  cout<<"night value:"<<nights<<" location:"<<&nights<<endl;
+
+  int *pt = new int;
+  *pt = 44;
+
+  double *dp = new double;
+  *dp = 10.0;
+
+  cout<<"size of pt:"<<sizeof(pt)<<endl;
+  cout<<"size of dp"<<sizeof(dp)<<endl;
+  cout<<"location of *dp"<<dp<<endl;
+  cout<<"location of pointer dp "<<&dp<<endl;
+}
+ */
