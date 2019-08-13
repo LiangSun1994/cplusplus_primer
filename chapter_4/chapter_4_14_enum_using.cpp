4f93d368a20fb27bb6963092b78b0ea012be7214
e <1373757850@qq.com>
2019-08-13 22:38:44 +0800

    add pointer

--- chapter_4/chapter_4_14_enum_using.cpp
+++ chapter_4/chapter_4_14_enum_using.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+/*
 int main(){
   enum Suit{liangsun, dongwan}a,b;
   a = liangsun;
@@ -9,3 +9,4 @@ int main(){
   //#1.枚举值常量不是变量，不能进行赋值
   //#2.枚举可以隐式的转化为int,但是int不能转化为enum
 }
+*/
---
+++ chapter_4/chapter_4_14_pointer.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+ int main(){
+   int donuts = 1;
+   double cups = 4.5;
+   cout<<"donuts:"<<donuts<<" address of donuts:"<<&donuts<<endl;
+   cout<<"cups:"<<cups<<" address of cups: "<<&cups<<endl;
+ }
+
