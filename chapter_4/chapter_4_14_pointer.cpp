8410d26c15b71e1734c6f4c55eda6e15a6b5b709
e <1373757850@qq.com>
2019-08-13 22:41:47 +0800

    x

--- chapter_4/chapter_4_14_pointer.cpp
+++ chapter_4/chapter_4_14_pointer.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-
+/*
 using namespace std;
  int main(){
    int donuts = 1;
    double cups = 4.5;
    cout<<"donuts:"<<donuts<<" address of donuts:"<<&donuts<<endl;
    cout<<"cups:"<<cups<<" address of cups: "<<&cups<<endl;
+   cout<<sizeof(cups)<<endl;
+   //两个变量不是相邻的
  }
-
+*/
