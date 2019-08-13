daf51c84439e1772510149df1481e9a7075392be
e <1373757850@qq.com>
2019-08-13 22:55:56 +0800

    two ways to get variable value

---
+++ chapter_4/chapter_4_15_pointer.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main(){
+  int update = 6;
+  int* p_update;
+  cout<<&p_update<<endl;
+  //#1.变量初始化后，指针变量的是有地址的，但是没有值
+  p_update = &update;
+  cout<<"feafefaef"<<endl;
+  cout<<&p_update<<endl;
+  cout<<*p_update<<endl;
+  //2.可以通过两种方式获得数值
+  cout<<"update:"<<update<<endl;
+  cout<<"*p_update:"<<*p_update<<endl;
+  //3.获取地址的两种方式
+  cout<<"address of update"<<&update<<endl;
+  cout<<"address of update"<<p_update<<endl;
+}
+*/
