18497154ce0f3040c55556a5433a33cac6f858b1
e <1373757850@qq.com>
2019-08-14 23:04:13 +0800

    add arraynew

---
+++ chapter_4/chapter_4_18_arraynew.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/*
+using namespace std;
+int main(){
+  double *p3 = new double[3];
+  p3[0] = 1.0;
+  p3[1] = 2.0;
+  p3[2] = 3.0;
+
+  cout<<"p[1]:"<<p3[1]<<endl;
+  p3 = p3+1;
+  cout<<"p[1]:"<<p3[1]<<endl;
+  p3 = p3 -1;
+  delete[] p3;
+}
+ */
