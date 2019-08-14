342943dd41777d685192c0d496293f11e646240a
e <1373757850@qq.com>
2019-08-14 23:09:25 +0800

    add add_pointer

---
+++ chapter_4/chapter_4_19_addpoiter.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+/*
+int main(){
+  using namespace std;
+
+  double wage[3] = {1.0,2.0,3.0};
+  short stacks[3] = {1,2,3};
+
+  double * wa = wage;
+  short * sh = &stacks[0];
+
+  cout<<"wa:"<<wa<< " *wa:"<<*wa<<endl;
+  ++wa;
+  cout<<"wa:"<<wa<< " *wa:"<<*wa<<endl;
+
+  cout<<"sh:"<<sh<<" *sh"<<*sh<<endl;
+  ++sh;
+  cout<<"sh:"<<sh<<" *sh"<<*sh<<endl;
+
+  cout<<"size of(wa):"<< sizeof(wa)<<endl;
+  cout<<"size of(sh):"<<sizeof(sh)<<endl;
+}
+ */
