4a419d41830bcb0071feb9d9995ac78e5162239e
LiangSun1994 <liangsun@pony.ai>
2019-08-20 17:29:17 +0800

    add arrfun4

---
+++ chapter_07/chapter_07_08_arrfun4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/*
+int sum(int * begin,int* end);
+int main(){
+  int arr[6] = {1,2,3,4,5,6};
+  cout<<sum(arr,arr+3)<<endl;
+}
+int sum(int * begin,int * end){
+  int res = 0;
+  const int* p;
+  //the pointer can can motify,but the value which the poiter point can not change.
+  for(p = begin;p!=end;++p){
+    res += *p;
+  }
+  return res;
+}
+
+ */
