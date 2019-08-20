c881590e19b1e8267e66777f1e0728f436cb50cc
LiangSun1994 <liangsun@pony.ai>
2019-08-20 16:01:49 +0800

    add arrfun

--- chapter_07/chapter_07_04_lotto.cpp
+++ chapter_07/chapter_07_04_lotto.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 /*
 long double computeLotto(int number,int pick);
 int main(){
---
+++ chapter_07/chapter_07_05_arrfun.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+/*
+const int ArrSize = 3;
+int sum(int arr[], int ArrSize);
+int main(){
+  int arr[] = {1,2,4};
+  cout<<sum(arr,ArrSize)<<endl;
+
+}
+
+int sum(int arr[], int ArrSize){
+  int res = 0;
+  for(int i = 0;i<ArrSize;i++){
+    res += arr[i];
+  }
+  return res;
+}
+ */
