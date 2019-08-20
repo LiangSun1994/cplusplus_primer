2e34cb6122bcfaebc2d5d7742bfad5b8f850ef82
LiangSun1994 <liangsun@pony.ai>
2019-08-20 16:25:07 +0800

    add arrfun2

--- chapter_07/chapter_07_05_arrfun.cpp
+++ chapter_07/chapter_07_05_arrfun.cpp
@@ -17,9 +17,11 @@ int main(){
 
 int sum(int arr[], int ArrSize){
   int res = 0;
+  cout<<"address of arr in function: "<<arr<<endl;
   for(int i = 0;i<ArrSize;i++){
     res += *(arr+i);
   }
   return res;
 }
-*/
+
+ */
---
+++ chapter_07/chapter_07_06_arrfun2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/*
+const int ArrSize = 4;
+int sum(int arr[], int ArrSize);
+int main(){
+  int arr[] = {1,2,3,4};
+  cout<<"Array address is "<<arr<<endl;
+  cout<<"size of arr in main"<< sizeof(arr)<<endl;
+  int res1 = sum(arr,ArrSize);
+  cout<<"sum of arr is "<< sum(arr,ArrSize)<<endl;
+}
+int sum(int arr[], int ArrSize){
+  int res = 0;
+  cout<<"address of arr"<<arr<<endl;
+  cout<<"size of arr in function:"<< sizeof(arr)<<endl;
+  for(int i = 0;i<ArrSize;i++){
+    res += arr[i];
+  }
+  return res;
+}
+*/
