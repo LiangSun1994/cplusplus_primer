6b7212f5f033a685033c74f6ea19b4bfa1139fd0
LiangSun1994 <liangsun@pony.ai>
2019-08-20 16:08:45 +0800

    modify arrfun

--- chapter_07/chapter_07_05_arrfun.cpp
+++ chapter_07/chapter_07_05_arrfun.cpp
@@ -6,14 +6,20 @@ int sum(int arr[], int ArrSize);
 int main(){
   int arr[] = {1,2,4};
   cout<<sum(arr,ArrSize)<<endl;
+  cout<<arr<<endl;
+  cout<<&arr<<endl;
+  //arr is address of array's first value
+  //&arr is address of whole array
+  cout<<arr+1<<endl;
+  cout<<(&arr)+1<<endl;
 
 }
 
 int sum(int arr[], int ArrSize){
   int res = 0;
   for(int i = 0;i<ArrSize;i++){
-    res += arr[i];
+    res += *(arr+i);
   }
   return res;
 }
- */
+*/
