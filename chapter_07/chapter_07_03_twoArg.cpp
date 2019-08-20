33d40048cd7828524cccff7475b028e497315d6e
LiangSun1994 <liangsun@pony.ai>
2019-08-20 15:50:20 +0800

    add twoArg and lotto

---
+++ chapter_07/chapter_07_03_twoArg.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+/*
+void char_times(char,int);
+int main(){
+  char c;
+  int time;
+  cout<<"please enter a char"<<endl;
+  cin >> c;
+  cout<<"please enter times"<<endl;
+  cin>>time;
+  while(c != 'q'){
+    char_times(c,time);
+    cin>>c;
+  }
+
+}
+void char_times(char c, int times) {
+  while(times > 0){
+    cout<<c<<endl;
+    --times;
+  }
+}
+*/
---
+++ chapter_07/chapter_07_04_lotto.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+/*
+long double computeLotto(int number,int pick);
+int main(){
+  int number = 5;
+  int pick = 2;
+  cout<<computeLotto(number,pick)<<endl;
+}
+
+long double computeLotto(int number,int pick){
+  long double res = 1.0;
+  int i;
+  int j;
+  for(i = number,j = pick; j>0;i--,j--){
+      res = res * i/j;
+  }
+  return res;
+}
+ */
