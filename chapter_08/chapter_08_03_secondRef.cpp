7ed50d2285e2ac39e12fcc71de511995e430f728
e <1373757850@qq.com>
2019-08-21 23:04:17 +0800

    add second ref

---
+++ chapter_08/chapter_08_03_secondRef.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+/*
+int main()
+{
+  int a =11;
+  int & b = a;
+
+  cout<< "address a :"<<&a<<endl;
+  cout<<"address b:"<<&b<<endl;
+
+  int c = 3;
+  b = c;
+  cout<<"a:"<<a<<"  address a:"<<&a<<endl;
+  cout<<"b:"<<b<<"  address b:"<<&b<<endl;
+  cout<<"c:"<<c<<"  address c:"<<&c<<endl;
+
+
+  return 0;
+}
+*/
