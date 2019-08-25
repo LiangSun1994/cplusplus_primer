465df18fd125c21849acbbac64d906625b379dd3
e <1373757850@qq.com>
2019-08-25 12:08:03 +0800

    add leftover

---
+++ chapter_08/chapter_08_10_leftover.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+/*
+unsigned long left(unsigned long source, unsigned int l);
+int main(){
+  unsigned long  source = 123456789;
+  for(int i =0 ;i< 18;i++){
+    cout<<left(source,i)<<endl;;
+  }
+}
+
+unsigned long left(unsigned long source, unsigned int l){
+  int digit = 1;
+  int s = source;
+  if(l <= 0 || source == 0){
+    return 0;
+  }
+  while(s /= 10){
+    ++digit;
+  }
+  int temp = digit - l;
+  if(digit > l){
+    while(temp--){
+      source /= 10;
+    }
+    return source;
+  }else{
+    return source;
+  }
+}
+
+ */
